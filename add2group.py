a71cfdabfbb9c2ac8c06a0187437e6e822ec2a59
ivam3 <ivam3.bh@gmail.com>
2021-10-06 22:23:58 -0500

    first commit

--- add2group.py
+++ add2group.py
@@ -51,77 +51,80 @@ if not client.is_user_authorized():
  
 os.system('clear')
 banner()
-input_file = sys.argv[1]
-users = []
-with open(input_file, encoding='UTF-8') as f:
-    rows = csv.reader(f,delimiter=",",lineterminator="\n")
-    next(rows, None)
-    for row in rows:
-        user = {}
-        user['username'] = row[0]
-        user['id'] = int(row[1])
-        user['access_hash'] = int(row[2])
-        user['name'] = row[3]
-        users.append(user)
- 
-chats = []
-last_date = None
-chunk_size = 200
-groups=[]
- 
-result = client(GetDialogsRequest(
-             offset_date=last_date,
-             offset_id=0,
-             offset_peer=InputPeerEmpty(),
-             limit=chunk_size,
-             hash = 0
-         ))
-chats.extend(result.chats)
- 
-for chat in chats:
-    try:
-        if chat.megagroup== True:
-            groups.append(chat)
-    except:
-        continue
+#ADDED
+try:
+    input_file = sys.argv[1]
+    users = []
+    with open(input_file, encoding='UTF-8') as f:
+        rows = csv.reader(f,delimiter=",",lineterminator="\n")
+        next(rows, None)
+        for row in rows:
+            user = {}
+            user['username'] = row[0]
+            user['id'] = int(row[1])
+            user['access_hash'] = int(row[2])
+            user['name'] = row[3]
+            users.append(user)
 
-print(cy+'(_>)─➤ Choose a group to add members')
-i=0
-for group in groups:
-    print(cy+' ╰─['+bl+str(i)+cy+']─➤ '+bl+group.title)
-    i+=1
+    chats = []
+    last_date = None
+    chunk_size = 200
+    groups=[]
 
-g_index = input("\n"+cy+"(_>)─➤"+bl+" Enter a Number : "+wi)
-target_group=groups[int(g_index)]
- 
-target_group_entity = InputPeerChannel(target_group.id,target_group.access_hash)
+    result = client(GetDialogsRequest(
+        offset_date=last_date,
+        offset_id=0,
+        offset_peer=InputPeerEmpty(),
+        limit=chunk_size,
+        hash = 0
+        ))
+    chats.extend(result.chats)
 
-print(cy+" ╰─["+bl+"1"+cy+"]─➤"+bl+" add member by user ID\n"+cy+" ╰─["+bl+"2"+cy+"]─➤"+bl+" add member by username ")
-mode = int(input(cy+"Input : "+wi)) 
-n = 0
- 
-for user in users:
-    n += 1
-    if n % 50 == 0:
-	    time.sleep(1)
-	    try:
-	        print ("Adding {}".format(user['id']))
-	        if mode == 1:
-	            if user['username'] == "":
-	                continue
-	            user_to_add = client.get_input_entity(user['username'])
-	        elif mode == 2:
-	            user_to_add = InputPeerUser(user['id'], user['access_hash'])
-	        else:
-	            sys.exit(re+"(_>)─➤ Invalid mode selected. Please try again.")
-	        client(InviteToChannelRequest(target_group_entity,[user_to_add]))
-	        print(cy+"(_>)─➤ Waiting for 5-10 Seconds...")
-	        time.sleep(random.randrange(5, 10))
-	    except PeerFloodError:
-	        print(re+"(_>)─➤ Getting flood error from Telegram. \n ╰──➤ Script is stopping now. \n ╰──➤ Please try again after some time.")
-	    except UserPrivacyRestrictedError:
-	        print(re+"(_>)─➤ The user's privacy settings do not allow you to do this. \n ╰──➤ Skipping.")
-	    except:
-	        traceback.print_exc()
-	        print(re+"(_>)─➤ Unexpected Error")
-	        continue
+    for chat in chats:
+        try:
+            if chat.megagroup== True:
+                groups.append(chat)
+        except:
+            continue
+
+    print(cy+'(_>)─➤ Choose a group to add members')
+    i=0
+    for group in groups:
+        print(cy+' ╰─['+bl+str(i)+cy+']─➤ '+bl+group.title)
+        i+=1
+
+    g_index = input("\n"+cy+"(_>)─➤"+bl+" Enter a Number : "+wi)
+    target_group=groups[int(g_index)]
+    target_group_entity = InputPeerChannel(target_group.id,target_group.access_hash)
+
+    print(cy+" ╰─["+bl+"1"+cy+"]─➤"+bl+" add member by user ID\n"+cy+" ╰─["+bl+"2"+cy+"]─➤"+bl+" add member by username ")
+    mode = int(input(cy+"Input : "+wi)) 
+    n = 0
+
+    for user in users:
+        n += 1
+        if n % 50 == 0:
+            time.sleep(1)
+            try:
+                print ("Adding {}".format(user['id']))
+                if mode == 1:
+                    if user['username'] == "":
+                        continue
+                    user_to_add = client.get_input_entity(user['username'])
+                elif mode == 2:
+                    user_to_add = InputPeerUser(user['id'], user['access_hash'])
+                else:
+                    sys.exit(re+"(_>)─➤ Invalid mode selected. Please try again.")
+                client(InviteToChannelRequest(target_group_entity,[user_to_add]))
+                print(cy+"(_>)─➤ Waiting for 5-10 Seconds...")
+                time.sleep(random.randrange(5, 10))
+            except PeerFloodError:
+                print(re+"(_>)─➤ Getting flood error from Telegram. \n ╰──➤ Script is stopping now. \n ╰──➤ Please try again after some time.")
+            except UserPrivacyRestrictedError:
+                print(re+"(_>)─➤ The user's privacy settings do not allow you to do this. \n ╰──➤ Skipping.")
+            except:
+                traceback.print_exc()
+                print(re+"(_>)─➤ Unexpected Error")
+                continue
+except IndexError:
+    print(re+"(_>)─➤ .csv file doesn't specified. \n ╰──➤ Aborting.")
--- sendmsg.py
+++ sendmsg.py
@@ -49,51 +49,53 @@ class main():
         
         os.system('clear')
         main.banner()
-        input_file = sys.argv[1]
-        users = []
-        with open(input_file, encoding='UTF-8') as f:
-            rows = csv.reader(f,delimiter=",",lineterminator="\n")
-            next(rows, None)
-            for row in rows:
-                user = {}
-                user['username'] = row[0]
-                user['id'] = int(row[1])
-                user['access_hash'] = int(row[2])
-                user['name'] = row[3]
-                users.append(user)
-        print(cy+'(_>)─➤ Choose a method')
-        print(cy+" ╰─["+bl+"1"+cy+"]─➤"+bl+" send message by user ID\n"+cy+" ╰─["+bl+"2"+cy+"]─➤"+bl+" send message by username ")
-        mode = int(input(cy+"Input : "+bl))
-         
-        message = input("\n"+cy+"(_>)─➤ Enter Your Message : "+wi)
-         
-        for user in users:
-            if mode == 2:
-                if user['username'] == "":
+        try:
+            input_file = sys.argv[1]
+            users = []
+            with open(input_file, encoding='UTF-8') as f:
+                rows = csv.reader(f,delimiter=",",lineterminator="\n")
+                next(rows, None)
+                for row in rows:
+                    user = {}
+                    user['username'] = row[0]
+                    user['id'] = int(row[1])
+                    user['access_hash'] = int(row[2])
+                    user['name'] = row[3]
+                    users.append(user)
+            print(cy+'(_>)─➤ Choose a method')
+            print(cy+" ╰─["+bl+"1"+cy+"]─➤"+bl+" send message by user ID\n"+cy+" ╰─["+bl+"2"+cy+"]─➤"+bl+" send message by username ")
+            mode = int(input(cy+"Input : "+bl))
+
+            message = input("\n"+cy+"(_>)─➤ Enter Your Message : "+wi)
+
+            for user in users:
+                if mode == 2:
+                    if user['username'] == "":
+                        continue
+                    receiver = client.get_input_entity(user['username'])
+                elif mode == 1:
+                    receiver = InputPeerUser(user['id'],user['access_hash'])
+                else:
+                    print(re+"(_>)─➤ Invalid Mode. Exiting.")
+                    client.disconnect()
+                    sys.exit()
+                try:
+                    print(cy+"(_>)─➤ Sending Message to"+bl+" :", user['name'])
+                    client.send_message(receiver, message.format(user['name']))
+                    print(ye+" ╰──➤ Waiting {} seconds".format(SLEEP_TIME))
+                    time.sleep(SLEEP_TIME)
+                except PeerFloodError:
+                    print(re+"(_>)─➤ Getting Flood Error from telegram. \n ╰──➤ Script is stopping now. \n ╰──➤ Please try again after some time.")
+                    client.disconnect()
+                    sys.exit()
+                except Exception as e:
+                    print(re+"(_>)─➤ Error:", e)
+                    print(re+" ╰──➤ Trying to continue...")
                     continue
-                receiver = client.get_input_entity(user['username'])
-            elif mode == 1:
-                receiver = InputPeerUser(user['id'],user['access_hash'])
-            else:
-                print(re+"(_>)─➤ Invalid Mode. Exiting.")
                 client.disconnect()
-                sys.exit()
-            try:
-                print(cy+"(_>)─➤ Sending Message to"+bl+" :", user['name'])
-                client.send_message(receiver, message.format(user['name']))
-                print(ye+" ╰──➤ Waiting {} seconds".format(SLEEP_TIME))
-                time.sleep(SLEEP_TIME)
-            except PeerFloodError:
-                print(re+"(_>)─➤ Getting Flood Error from telegram. \n ╰──➤ Script is stopping now. \n ╰──➤ Please try again after some time.")
-                client.disconnect()
-                sys.exit()
-            except Exception as e:
-                print(re+"(_>)─➤ Error:", e)
-                print(re+" ╰──➤ Trying to continue...")
-                continue
-        client.disconnect()
-        print(gr+"Done. Message sent to all users.")
+                print(gr+"Done. Message sent to all users.")
 
-
+        except IndexError:
+            print(re+"(_>)─➤ .csv file doesn't specified. \n ╰──➤ Aborting.")
 
 main.send_sms()
